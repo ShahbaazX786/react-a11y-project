dbaaaff871332154180cc51316f4fc411ae19df2
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-31 11:35:40 +0530

    fix: build failed due to type error.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // @ts-expect-error
   test: {
     globals: true,
     environment: "jsdom",
