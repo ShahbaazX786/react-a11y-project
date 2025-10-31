c3a74132952bc29deb2425163cf26294cdb1fac3
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-31 11:24:26 +0530

    feat: Install and configure vite code coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+/// <reference types="vitest" />
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+    },
+  },
 });
