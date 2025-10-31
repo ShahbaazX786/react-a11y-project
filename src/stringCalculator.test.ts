7426ce725911e34a71666ed9ab66728cfa4968f3
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-31 07:47:32 +0530

    test: Add new test.

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -2,7 +2,10 @@ import { describe, expect, it } from "vitest";
 import evalExpression from "./stringCalculator";
 
 describe("String Calculator", () => {
-  it("should return single number", () => {
+  it("should return a single number", () => {
     expect(evalExpression("5")).toBe(5);
+  });
+  it("should not return empty string", () => {
+    expect(evalExpression("")).not.toBe("");
   });
 });
