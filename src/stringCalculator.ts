84dfe57f3e20537527ceb7f12c5454b7e0b51ee3
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-31 09:22:04 +0530

    add logic to handle arithmetic expressions in the string.

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,16 +1,46 @@
 const evalExpression = (input: string): number => {
   if (!input) return 0;
 
-  const sanitizedInput = input.replace(/\n/g, ",");
+  const sanitizedInput = input.replace(/\n/g, ",").replace(/\s+/g, "");
+  const sanitizedExpressions = sanitizedInput.split(",");
 
-  return sanitizedInput
-    .split(",")
-    .map((x) => x.trim())
-    .reduce((acc, curr) => {
-      const tempNumber = Number(curr);
-      const currNumber = isNaN(tempNumber) ? 0 : tempNumber;
-      return acc + currNumber;
-    }, 0);
+  return sanitizedExpressions.reduce(
+    (acc, curr) => acc + evalArithmetic(curr),
+    0
+  );
+};
+
+const evalArithmetic = (expression: string) => {
+  const numberStack: number[] = [];
+  let currentOperator: string = "+";
+
+  if (!expression) return 0;
+
+  const tokens = expression.match(/(\d+\.?\d*|[+\-*/])/g);
+  if (!tokens) return 0;
+
+  for (const token of tokens) {
+    if (/[+\-*/]/.test(token)) {
+      currentOperator = token;
+    } else {
+      const num = Number(token);
+      switch (currentOperator) {
+        case "+":
+          numberStack.push(num);
+          break;
+        case "-":
+          numberStack.push(-num);
+          break;
+        case "*":
+          numberStack.push(numberStack.pop()! * num);
+          break;
+        case "/":
+          numberStack.push(numberStack.pop()! / num);
+          break;
+      }
+    }
+  }
+  return numberStack.reduce((a, b) => a + b, 0);
 };
 
 export default evalExpression;
