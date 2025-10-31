6c0fa184d1d56136b229c0f977be128d6c79ee3c
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-31 08:30:02 +0530

    test: Add alphabetic invasion in numerical string.

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -18,6 +18,10 @@ describe("String Calculator", () => {
 
   it("should sum by default and return result of two or more numbers", () => {
     expect(evalExpression("5, 10")).toBe(15);
+    expect(evalExpression("5,b,10")).toBe(15);
+    expect(evalExpression("a,b,10")).toBe(10);
+    expect(evalExpression("a,b")).toBe(0);
+    expect(evalExpression("a,5,c")).toBe(5);
     expect(evalExpression("5,10,10")).not.toBe(0);
     expect(evalExpression("5,10,10")).not.toBe(undefined);
     expect(evalExpression("5,10,10")).not.toBe(null);
