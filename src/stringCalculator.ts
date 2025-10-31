43064eadadd551b0e851f0f001692be9f8376526
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-31 08:16:01 +0530

    Add sum of numbers when comma is in input.

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,12 @@
 const evalExpression = (input: string): number => {
   if (!input) return 0;
+
+  if (input && input.includes(",")) {
+    return input.split(",").reduce((acc, curr) => {
+      return acc + Number(curr);
+    }, 0);
+  }
+
   return +input;
 };
 
